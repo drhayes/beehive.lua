41299aa483e49b2f907e3c3af1a9d11d91a733b1
David Hayes <drhayes@gmail.com>
2020-07-24 12:40:44 -0500

    Add repeat tests.

---
+++ test/repeat_spec.lua
@@ -0,0 +1,52 @@
+local repeatIt = require 'beehive.repeat'
+
+local successCount = 0
+local function success()
+  successCount = successCount + 1
+  return 'success'
+end
+
+local failCount = 0
+local function fail()
+  failCount = failCount + 1
+  return 'failure'
+end
+
+describe('repeat', function()
+  before_each(function()
+    successCount = 0
+    failCount = 0
+  end)
+
+  it('repeats upon success', function()
+    local tree = repeatIt(5, success)
+    local result = tree()
+    assert.are.equal(result, 'running')
+    assert.are.equal(1, successCount)
+    result = tree()
+    assert.are.equal(result, 'running')
+    assert.are.equal(2, successCount)
+    result = tree()
+    assert.are.equal(result, 'running')
+    assert.are.equal(3, successCount)
+    result = tree()
+    assert.are.equal(result, 'running')
+    assert.are.equal(4, successCount)
+    result = tree()
+    assert.are.equal(result, 'success')
+    assert.are.equal(5, successCount)
+  end)
+
+  it('just keeps on failing', function()
+    local tree = repeatIt(5, fail)
+    local result = tree()
+    assert.are.equal(result, 'failure')
+    assert.are.equal(1, failCount)
+    for i = 2, 10 do
+      result = tree()
+      assert.are.equal(result, 'failure')
+      assert.are.equal(i, failCount)
+    end
+  end)
+end)
+
